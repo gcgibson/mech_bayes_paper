1cb1f005594450f8483efc8065164c6601e1ec2d
graham gibson <gcgibson@umass.edu>
2020-08-19 07:55:24 -0600

    update

--- data.R
+++ data.R
@@ -1,19 +1,32 @@
 library(stringr)
 library(dplyr)
-deaths <- read.csv("/Users/gcgibson/Desktop/truth-Incident Deaths.txt")  # Number of students in bed
+library(RCurl)
+#deaths <- read.csv("/Users/gcgibson/Desktop/truth-Incident Deaths.txt")  # Number of students in bed
+x <- getURL("https://raw.githubusercontent.com/reichlab/covid19-forecast-hub/master/data-truth/truth-Cumulative%20Deaths.csv")
+y <- read.csv(text = x)
+deaths <- y[!grepl("County",y$location_name) , ]
 
 
 
 state_deaths <- deaths %>%
   filter(!str_detect(location_name, "County"))
-state_deaths$date <- as.Date(state_deaths$adate)
+state_deaths$date <- as.Date(state_deaths$date)
+state_deaths <- state_deaths %>% group_by(location_name) %>%mutate(incident=c(value[1],diff(value))) %>% ungroup()
 
-state_deaths_subset_idx  <- unique(state_deaths[state_deaths$value >= 50,]$location_name)
+state_deaths_subset_idx  <- unique(state_deaths[state_deaths$incident >= 50,]$location_name)
 
 state_deaths_subset <- state_deaths[state_deaths$location_name %in% state_deaths_subset_idx,]
 library(ggplot2)
 
-data_plot <- ggplot(state_deaths_subset,aes(x=date,y=value)) + geom_point(size=.5) + facet_wrap(~location_name,scales="free") + theme_bw() +ylab("Incident Deaths") + xlab("Date")
+
+state_deaths_subset_total_deaths <- state_deaths_subset %>% group_by(location) %>% mutate(total_deaths = sum(value)) %>% ungroup()
+state_deaths_subset <- left_join(state_deaths_subset,state_deaths_subset_total_deaths)
+state_deaths_subset$total_deaths <- as.factor(state_deaths_subset$total_deaths)
+state_deaths_subset$total_deaths <- factor(state_deaths_subset$total_deaths,levels = rev(levels(state_deaths_subset$total_deaths)))
+state_deaths_subset$location_name <- as.factor(state_deaths_subset$location_name)
+state_deaths_subset <- state_deaths_subset[order(state_deaths_subset$total_deaths),]
+state_deaths_subset$location_name <- factor(state_deaths_subset$location_name,levels = unique(state_deaths_subset$location_name))
+data_plot <- ggplot(state_deaths_subset,aes(x=date,y=incident)) + geom_point(size=.5) + facet_wrap(~location_name,scales="free") + theme_bw() +ylab("Incident Reported COVID-19 Deaths (per day)") + xlab("")
 ggsave("/Users/gcgibson/mech_bayes_paper/data_plot.png",data_plot,device="png",width=10,height=10)
 
 
@@ -21,16 +34,18 @@ ggsave("/Users/gcgibson/mech_bayes_paper/data_plot.png",data_plot,device="png",w
 library(reticulate)
 np <- import("numpy")
 files = list.files("/Users/gcgibson/mech_bayes_paper/samples/samples")
-fit_plot_df <- array(NA,dim=c(length(files),3,144) )
+fit_plot_df <- array(NA,dim=c(length(files),3,172) )
 col_idx <- 1
 states = c()
 for (file in files){
     print (file)
     npz1 <- np$load(paste0("/Users/gcgibson/mech_bayes_paper/samples/samples/",file),allow_pickle = T)
     tmp <- npz1$f$post_pred_samples[[1]]$dz
+    tmp_fcast <- npz1$f$forecast_samples[[1]]$dz_future
+    tmp <- cbind(tmp,tmp_fcast)
     tmp_med <- apply(tmp,2,median)
-    tmp_f <- function(x)return(quantile(x,probs = .95))
-    tmp_f2 <- function(x)return(quantile(x,probs = .05))
+    tmp_f <- function(x)return(quantile(x,probs = .975))
+    tmp_f2 <- function(x)return(quantile(x,probs = .025))
     
     tmp_95 <- apply(tmp,2,tmp_f)
     tmp_05 <- apply(tmp,2,tmp_f2)
@@ -46,16 +61,28 @@ for (file in files){
   
 }
 
-plot_df <- data.frame(time=rep(seq(as.Date("2020-03-05"),as.Date("2020-07-26"),by="day"),length(states)),
+plot_df <- data.frame(time=rep(seq(as.Date("2020-03-05"),as.Date("2020-08-23"),by="day"),length(states)),
                       median=c(t(fit_plot_df[,1,])),upper=c(t(fit_plot_df[,2,])),
                       lower=c(t(fit_plot_df[,3,])))
-plot_df$location <- rep(states,each=144)
+plot_df$location <- rep(states,each=172)
 plot_df_subset <- plot_df[plot_df$location %in% c("US",state.abb[match(state_deaths_subset_idx,state.name)]), ]
 state_deaths_subset_for_addition <- state_deaths_subset#[state_deaths_subset$location_name != "New Jersey",]
 state_deaths_subset_for_addition <- state_deaths_subset_for_addition[state_deaths_subset_for_addition$date %in% plot_df_subset$time,]
 state_deaths_subset_for_addition$location <- state.abb[match(state_deaths_subset_for_addition$location_name,state.name)]
 state_deaths_subset_for_addition[is.na(state_deaths_subset_for_addition$location),]$location <- "US"
+state_deaths_subset_for_addition <- state_deaths_subset_for_addition[order(state_deaths_subset_for_addition$total_deaths),]
+state_deaths_subset_for_addition$location_name <- factor(state_deaths_subset_for_addition$location_name,levels = unique(state_deaths_subset_for_addition$location_name))
+
+plot_df_subset$location_name <- state.name[match(plot_df_subset$location, state.abb)]
+plot_df_subset$location_name  <- factor(plot_df_subset$location_name ,levels=unique(state_deaths_subset_for_addition$location_name))
+plot_df_subset[is.na(plot_df_subset$location_name), ]$location_name <- "US"
 fit_and_forecast_results <- ggplot(plot_df_subset,aes(x=time,y=median)) + geom_line(col="cornflowerblue") +
-  geom_ribbon(aes(ymin=lower, ymax=upper),alpha=0.2,size=0,colour="cornflowerblue")+geom_point(data=state_deaths_subset_for_addition,aes(x=date,y=value),size=.5)  +facet_wrap(~location,scales='free') + theme_bw() 
+  geom_ribbon(aes(ymin=lower, ymax=upper),alpha=0.2,size=0,colour="cornflowerblue")+geom_point(data=state_deaths_subset_for_addition,aes(x=date,y=incident),size=.5)  +facet_wrap(~location_name,scales='free') +
+  theme_bw() + geom_vline(xintercept = as.Date("2020-07-26"),alpha=.75)
 ggsave("/Users/gcgibson/mech_bayes_paper/fit_and_forecast_results.png",fit_and_forecast_results,device="png",width=8,height=8)
 
+
+plot_df_subset$date <- plot_df_subset$time
+coverage_df <- plot_df_subset %>% left_join(state_deaths_subset_for_addition,by=c("date","location"))
+coverage_df <- coverage_df %>% group_by(location,date) %>% mutate(cov = ifelse(lower <= incident & incident <= upper,1,0))
+sum(coverage_df$cov,na.rm=T)/length(coverage_df$cov)
--- score.R
+++ score.R
@@ -4,12 +4,15 @@ library(lubridate)
 zoltar_connection <- new_connection()
 zoltar_authenticate(zoltar_connection, "gcgibson", "casey!ili")
 ## read in fips
+fips_csv <- read.csv(url("https://raw.githubusercontent.com/reichlab/covid19-forecast-hub/master/data-locations/locations.csv"))
 
 ## access scores for the COVID-19 Forecasts project
 tmp <- scores(zoltar_connection, "https://www.zoltardata.com/api/project/44/") %>%
 
   mutate(wis = (abs_error+.01*interval_2+.025*interval_5+.05*interval_10+.1*interval_20+.15*interval_30+.2*interval_40+.25*interval_50+.3*interval_60+.35*interval_70+.40*interval_80+.45*interval_90)/12,
          mae=abs_error,error=error)
+
+tmp <- tmp[tmp$target %in% paste0(1:4, " wk ahead inc death"),]
 tmp_mech_bayes <- tmp[tmp$model %in%  c("UMass-MechBayes","COVIDhub-baseline"),]
 tmp_mech_bayes$timezero <- as.Date(tmp_mech_bayes$timezero)
 #tmp_mech_bayes <- tmp_mech_baye
@@ -20,15 +23,24 @@ tmp_subset <- tmp_mech_bayes %>%  dplyr::group_by(timezero,unit,model,target) %>
 tmp_subset <- tmp_subset[tmp_subset$timezero > "2020-04-20",]
 tmp_subset[tmp_subset$model == "COVIDhub-baseline",]$timezero <- tmp_subset[tmp_subset$model == "COVIDhub-baseline",]$timezero -1
 library(ggplot2)
-mae_results_by_time_zero <- ggplot(tmp_subset %>% group_by(timezero,model) %>% summarize(mae=mean(mae)),aes(x=timezero,y=mae,col=model)) + geom_point() + theme_bw() +  theme(axis.text.x = element_text(angle = 90)) + 
-  geom_vline(xintercept=as.numeric(as.Date("2020-05-10")),linetype=4) + geom_vline(xintercept=as.numeric(as.Date("2020-05-24")),linetype=4) +
-  annotate("text", x =as.Date("2020-05-08"), y = 400, label = "V1") +  annotate("text", x =as.Date("2020-05-22"), y = 400, label = "V2") +   annotate("text", x =as.Date("2020-07-22"), y = 400, label = "V3")
+mae_results_by_time_zero <- ggplot(tmp_subset[tmp_subset$timezero >= "2020-05-10",] %>% group_by(timezero,model) %>% summarize(mae=mean(mae)),aes(x=timezero,y=mae,col=model)) + geom_point() + theme_bw() +  theme(axis.text.x = element_text(angle = 90)) + 
+  #annotate("text", x =as.Date("2020-04-15"), y = 150, label = "V1") + 
+  annotate("text", x =as.Date("2020-05-12"), y = 150, label = "V1") +   
+  annotate("text", x =as.Date("2020-07-01"), y = 150, label = "V2") + 
+  #geom_ribbon(aes(xmin =as.Date("2020-04-05"),xmax=as.Date("2020-05-05") ),alpha=.1,fill='thistle1',colour='thistle1') +
+  geom_ribbon(aes(xmin =as.Date("2020-05-05"),xmax=as.Date("2020-05-20") ),alpha=.1,fill='thistle3',colour='thistle3') +
+  geom_ribbon(aes(xmin =as.Date("2020-05-20"),xmax=as.Date("2020-08-20") ),alpha=.1,fill='thistle4',colour='thistle4') + xlab("Date")
+  
 
 
 ggsave("/Users/gcgibson/mech_bayes_paper/mae_results_by_time_zero.png",mae_results_by_time_zero,device="png",width=8,height=4)
 
+fips_csv$unit <- fips_csv$location
+tmp_subset_r <- tmp_subset %>% group_by(unit,model) %>% summarize(mae=mean(mae)) %>% left_join(fips_csv,by='unit')
+tmp_subset_r <- tmp_subset_r[rev(order(tmp_subset_r$mae)),]
+tmp_subset_r$abbreviation <- factor(tmp_subset_r$abbreviation,levels=unique(tmp_subset_r$abbreviation))
+mae_results_by_region <- ggplot(tmp_subset_r,aes(x=abbreviation,y=mae,col=model)) + geom_point() + theme_bw() +  theme(axis.text.x = element_text(angle = 90)) + xlab("State") + ylab("MAE")
 
-mae_results_by_region <- ggplot(tmp_subset %>% group_by(unit,model) %>% summarize(mae=mean(mae)),aes(x=unit,y=mae,col=model)) + geom_point() + theme_bw() +  theme(axis.text.x = element_text(angle = 90))
 ggsave("/Users/gcgibson/mech_bayes_paper/mae_results_by_region.png",mae_results_by_region,device="png",width=8,height=4)
 
 
@@ -46,14 +58,22 @@ mae_results_by_target <- ggplot(tmp_subset %>% group_by(target,model) %>% summar
 ggsave("/Users/gcgibson/mech_bayes_paper/mae_results_by_target.png",mae_results_by_target,device="png",width=8,height=4)
 
 
-wis_results_by_time_zero <- ggplot(tmp_subset %>% group_by(timezero,model) %>% summarize(wis=mean(wis)),aes(x=timezero,y=wis,col=model)) + geom_point() + theme_bw() +  theme(axis.text.x = element_text(angle = 90)) + geom_vline(xintercept=as.numeric(as.Date("2020-05-10")),linetype=4) + geom_vline(xintercept=as.numeric(as.Date("2020-05-24")),linetype=4) +
-  geom_vline(xintercept=as.numeric(as.Date("2020-05-10")),linetype=4) + geom_vline(xintercept=as.numeric(as.Date("2020-05-24")),linetype=4) +
-  annotate("text", x =as.Date("2020-05-08"), y = 400, label = "V1") +  annotate("text", x =as.Date("2020-05-22"), y = 400, label = "V2") +   annotate("text", x =as.Date("2020-07-22"), y = 400, label = "V3")
+wis_results_by_time_zero <- ggplot(tmp_subset[tmp_subset$timezero >= "2020-05-10",] %>% group_by(timezero,model) %>% summarize(wis=mean(wis)),aes(x=timezero,y=wis,col=model)) + geom_point() + theme_bw() +  theme(axis.text.x = element_text(angle = 90)) +
+  #annotate("text", x =as.Date("2020-04-15"), y = 100, label = "V1") + 
+  annotate("text", x =as.Date("2020-05-12"), y = 100, label = "V1") +   
+  annotate("text", x =as.Date("2020-07-01"), y = 100, label = "V2") + 
+  #geom_ribbon(aes(xmin =as.Date("2020-04-05"),xmax=as.Date("2020-05-05") ),alpha=.1,fill='thistle1',colour='thistle1') +
+  geom_ribbon(aes(xmin =as.Date("2020-05-05"),xmax=as.Date("2020-05-20") ),alpha=.1,fill='thistle3',colour='thistle3') +
+  geom_ribbon(aes(xmin =as.Date("2020-05-20"),xmax=as.Date("2020-08-20") ),alpha=.1,fill='thistle4',colour='thistle4') + xlab("Date")
 
 ggsave("/Users/gcgibson/mech_bayes_paper/wis_results_by_time_zero.png",wis_results_by_time_zero,device="png",width=8,height=4)
 
 
-wis_results_by_region <- ggplot(tmp_subset %>% group_by(unit,model) %>% summarize(wis=mean(wis)),aes(x=unit,y=wis,col=model)) + geom_point() + theme_bw() +  theme(axis.text.x = element_text(angle = 90))
+tmp_subset_rw <- tmp_subset %>% group_by(unit,model) %>% summarize(wis=mean(wis))%>% left_join(fips_csv,by='unit')
+tmp_subset_rw <- tmp_subset_rw[rev(order(tmp_subset_rw$wis)),]
+tmp_subset_rw$abbreviation <- factor(tmp_subset_rw$abbreviation,levels=unique(tmp_subset_rw$abbreviation))
+
+wis_results_by_region <- ggplot(tmp_subset_rw,aes(x=abbreviation,y=wis,col=model)) + geom_point() + theme_bw() +  theme(axis.text.x = element_text(angle = 90)) + xlab("State")
 ggsave("/Users/gcgibson/mech_bayes_paper/wis_results_by_region.png",wis_results_by_region,device="png",width=8,height=4)
 
 
